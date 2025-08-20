9796f340ba5d8520532b7a4a2d8188c9d3ebdf2d
Angelo L. Salenga <alsalenga@asyadesign.local>
2025-08-20 10:57:20 +0800

    Updates

--- static/js/611.7a60a810.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject=self.webpackChunkproject||[]).push([[611],{611:function(e,c,t){t.r(c),c.default={}}}]);
-//# sourceMappingURL=611.7a60a810.chunk.js.map
---
+++ static/js/784.09729b49.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproject=self.webpackChunkproject||[]).push([[784],{784:(e,c,s)=>{s.r(c),s.d(c,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=784.09729b49.chunk.js.map
